da8da9728d4473fcf2e817f7eca1427039059480
jinpeng1018 <191040707@qq.com>
2026-06-13 13:23:08 +0800

    Update download.py

--- dataset/download.py
+++ dataset/download.py
@@ -1,5 +1,5 @@
 The simulation dataset can be downloaded from: https://github.com/popnut123/TacDiffusion
 
-The simulation dataset can be downloaded from: https://github.com/popnut123/TacDiffusion
+The simulation dataset can be downloaded from: https://huggingface.co/datasets/jinpeng1018/HRFDiff/tree/main
 
 The dataset will be released after organization.
